c7c9f6d7c256c6306b252395ef91397d78905254
Dmitry Jum <xfiler@ymail.com>
2019-03-16 02:53:59 -0400

    adds search api method

--- src/utils/api.js
+++ src/utils/api.js
@@ -2,8 +2,7 @@ import axios from 'axios'
 import * as constants from "../constants/env/us_states_api";
 
 class USUApi {
-  static async getSchools(params) {
-    params = params || {}
+  static async getSchools(params={}) {
     return axios.get(`${constants.US_STATE_UNIVERSITIES_LOCAL_HOST}/api/v1/schools`, {
       headers: { 'Accept': 'application/json' },
       params: params
@@ -13,6 +12,13 @@ class USUApi {
   static async getTopTwentyKeys() {
     return axios.get(`${constants.US_STATE_UNIVERSITIES_LOCAL_HOST}/api/v1/schools/top_twenty_keys`, {
       headers: {'Accept': 'application/json'}
+    })
+  }
+
+  static async search(params={'term': ''}) {
+    return axios.get(`${constants.US_STATE_UNIVERSITIES_LOCAL_HOST}/api/v1/schools/search`, {
+      headers: { 'Accept': 'application/json' },
+      params: params
     })
   }
 }
