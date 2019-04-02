ec5be7543563b79c5d83c733e1107ea5cfd45093
Dmitry Jum <xfiler@ymail.com>
2019-04-02 00:43:56 -0400

    WIP: styled sign up form

--- src/components/signUp/index.js
+++ src/components/signUp/index.js
@@ -1,35 +1,38 @@
 import React, { Component } from 'react';
-import { Form, Button } from 'react-bootstrap';
+import { Form, Button, Jumbotron, Col } from 'react-bootstrap';
+import "./index.css";
 
 class SignUp extends Component {
 
   render() {
     return (
-      <>
-        <Form>
-          <Form.Group controlId="formBasicEmail">
-            <Form.Label>Email address</Form.Label>
-            <Form.Control type="email" placeholder="Enter email" />
-            <Form.Text className="text-muted">
-              We'll never share your email with anyone else.
-            </Form.Text>
-          </Form.Group>
+      <Col md={{span: 8, offset: 2}}>
+        <Jumbotron className="sign-up-form">
+          <Form>
+            <Form.Group controlId="formBasicEmail">
+              <Form.Label>Email address</Form.Label>
+              <Form.Control type="email" placeholder="Enter email" />
+              <Form.Text className="text-muted">
+                We'll never share your email with anyone else.
+              </Form.Text>
+            </Form.Group>
 
-          <Form.Group controlId="formBasicPassword">
-            <Form.Label>Password</Form.Label>
-            <Form.Control type="password" placeholder="Password" />
-          </Form.Group>
+            <Form.Group controlId="formBasicPassword">
+              <Form.Label>Password</Form.Label>
+              <Form.Control type="password" placeholder="Password" />
+            </Form.Group>
 
-          <Form.Group controlId="formBasicPassword">
-            <Form.Label>Password Confirmation</Form.Label>
-            <Form.Control type="password" placeholder="Password Confirmation" />
-          </Form.Group>
+            <Form.Group controlId="formBasicPasswordConfirmation">
+              <Form.Label>Password Confirmation</Form.Label>
+              <Form.Control type="password" placeholder="Password Confirmation" />
+            </Form.Group>
 
-          <Button variant="primary" type="submit">
-            Submit
-          </Button>
-        </Form>
-      </>
+            <Button variant="primary" type="submit">
+              Submit
+            </Button>
+          </Form>
+        </Jumbotron>
+      </Col>
     );
   }
 }
