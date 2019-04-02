a17ea40da9936bc827873d2eac5e765b88b6ed68
Dmitry Jum <xfiler@ymail.com>
2019-04-01 23:52:16 -0400

    WIP:Adds sign up form, needs styling and logic hook up

--- src/components/signUp/index.js
+++ src/components/signUp/index.js
@@ -1,12 +1,35 @@
 import React, { Component } from 'react';
+import { Form, Button } from 'react-bootstrap';
 
 class SignUp extends Component {
 
   render() {
     return (
-      <div>
-        <h2>Sign Up Page</h2>
-      </div>
+      <>
+        <Form>
+          <Form.Group controlId="formBasicEmail">
+            <Form.Label>Email address</Form.Label>
+            <Form.Control type="email" placeholder="Enter email" />
+            <Form.Text className="text-muted">
+              We'll never share your email with anyone else.
+            </Form.Text>
+          </Form.Group>
+
+          <Form.Group controlId="formBasicPassword">
+            <Form.Label>Password</Form.Label>
+            <Form.Control type="password" placeholder="Password" />
+          </Form.Group>
+
+          <Form.Group controlId="formBasicPassword">
+            <Form.Label>Password Confirmation</Form.Label>
+            <Form.Control type="password" placeholder="Password Confirmation" />
+          </Form.Group>
+
+          <Button variant="primary" type="submit">
+            Submit
+          </Button>
+        </Form>
+      </>
     );
   }
 }
