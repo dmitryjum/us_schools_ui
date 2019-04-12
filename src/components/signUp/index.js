9ba7731c8b81f70305289bb01947fbff69772077
Dmitry Jum <xfiler@ymail.com>
2019-04-11 22:13:22 -0400

    WIP:Adds refs to signup form and submit handler

--- src/components/signUp/index.js
+++ src/components/signUp/index.js
@@ -3,15 +3,32 @@ import { Form, Button, Jumbotron, Col } from 'react-bootstrap';
 import "./index.css";
 
 class SignUp extends Component {
+  constructor(props) {
+    super(props)
+    this.emailRef = React.createRef();
+    this.passwordRef = React.createRef();
+    this.passConfirmRef = React.createRef();
+  }
+
+  handleSubmit(e){
+    e.preventDefault()
+    console.log(this.emailRef.current.value)
+    console.log(this.passwordRef.current.value)
+    console.log(this.passConfirmRef.current.value)
+  }
 
   render() {
     return (
       <Col md={{span: 8, offset: 2}}>
         <Jumbotron className="sign-up-form">
-          <Form>
+          <Form onSubmit={(e) => this.handleSubmit(e)}>
             <Form.Group controlId="formBasicEmail">
               <Form.Label>Email address</Form.Label>
-              <Form.Control type="email" placeholder="Enter email" />
+              <Form.Control
+                type="email"
+                placeholder="Enter email"
+                ref={this.emailRef}
+              />
               <Form.Text className="text-muted">
                 We'll never share your email with anyone else.
               </Form.Text>
@@ -19,12 +36,20 @@ class SignUp extends Component {
 
             <Form.Group controlId="formBasicPassword">
               <Form.Label>Password</Form.Label>
-              <Form.Control type="password" placeholder="Password" />
+              <Form.Control
+                type="password"
+                placeholder="Password"
+                ref={this.passwordRef}
+              />
             </Form.Group>
 
             <Form.Group controlId="formBasicPasswordConfirmation">
               <Form.Label>Password Confirmation</Form.Label>
-              <Form.Control type="password" placeholder="Password Confirmation" />
+              <Form.Control
+                type="password"
+                placeholder="Password Confirmation"
+                ref={this.passConfirmRef}
+              />
             </Form.Group>
 
             <Button variant="primary" type="submit">
