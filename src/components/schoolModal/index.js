9ab2751900459449d4d884877bb699dd168454a3
Dmitry Jum <xfiler@ymail.com>
2020-04-08 18:01:01 -0400

    removes useless comments, adds a row on button click

--- src/components/schoolModal/index.js
+++ src/components/schoolModal/index.js
@@ -1,34 +1,32 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import { Modal, Button, FormControl, InputGroup, Badge, Row, Col } from 'react-bootstrap';
 import { useSelector, useDispatch } from 'react-redux';
-import { closeModal, updateSchool, openModal, setOldDetailsKey } from "../../actions/schoolModal";
+import { closeModal, updateSchool, openModal } from "../../actions/schoolModal";
 
 const SchoolModal = () => {
   const modalShow = useSelector(state => state.schoolModal.show);
   const school = useSelector(state => state.schoolModal.school);
   const user = useSelector(state => state.user.data.currentUser);
-  const oldDetailsKey = useSelector(state => state.schoolModal.oldDetailsKey);
-  const [schoolDetails, setSchoolDetails] = useState({});
   const dispatch = useDispatch();
   const handleClose = () => dispatch(closeModal());
-  // const [oldKey, setOldKey] = useState("")
   let timer = null
   let oldKey = ""
   // 2. Try to preserve search or filter state after School update
   // 3. Allow edit school title, if school got initial title '' from state
   // 5. Create a button to add a line with 2 input fields for key/value pair for details column
-  // 6. That button should just add '': '' pair to details state object and the component will rerender
   // 7. The component should defirintiate new school with no id in state and edited school with existing id in state
   // 8. Make sure school list updates with correct page and filters when school is updated or created
-  // useEffect(() => {
-  //   setSchoolDetails(school.details)
-  // }, [school.details, schoolDetails])
 
   function handleSubmit(e) {
     e.preventDefault()
     school.details["last edited by"] = user.email;
-    // setSchoolDetails(Object.assign(schoolDetails, { "last edited by": user.email }))
     dispatch(updateSchool({auth_token: user.auth_token, school}))
+  }
+
+  const addARow = () => {
+    school.details[""] = ""
+    dispatch(closeModal())
+    dispatch(openModal({ school }))
   }
 
   function onTimedChange(e, handler) {
@@ -43,27 +41,18 @@ const SchoolModal = () => {
     const currentValue = school.details[oldKey];
     delete school.details[oldKey];
     school.details[e.target.value] = currentValue;
-    // Object.assign(school, { details: school.details });
     dispatch(openModal({ school }));
-    // dispatch(setOldDetailsKey(e.target.value));
     oldKey = e.target.value
-
-    // setSchoolDetails(Object.assign(schoolDetails, { [e.target.value]: currentValue }))
-    // setOldKey(e.target.value)
   }
 
   const handleValueChange = (e) => {
     const nearestKey = e.target.closest('div.row').querySelector("input[name='Key']").value
     school.details[nearestKey] = e.target.value;
-    // Object.assign(school, { details: school.details });
     dispatch(openModal({ school }));
-    // setSchoolDetails(Object.assign(schoolDetails, { [nearestKey]: e.target.value }))
   }
 
   const handleOnFocus = (e) => {
-    // dispatch(setOldDetailsKey(e.target.value));
     oldKey = e.target.value
-    // setOldKey(e.target.value)
   }
 
   return (
@@ -122,6 +111,9 @@ const SchoolModal = () => {
       <Modal.Footer>
         <Badge pill variant="info"><i>Edited by:</i> {school.details["last edited by"]}</Badge>
         <Badge pill variant="secondary"><i>On:</i> {new Date(Date.parse(school.updated_at)).toDateString()}</Badge>
+        <Button variant="info"  onClick={addARow}>
+          Add a row
+        </Button>
         <Button variant="primary" type="submit" onClick={(e) => handleSubmit(e)}>
           Submit
         </Button>
