073114a7d62a9be38411d7aa0c8780462811abb6
Dmitry Jum <xfiler@ymail.com>
2023-03-17 00:46:48 -0700

    removes unused App test. Refactores the search school test to the most workable way. No need to mock store, because mockStore doesn't mock the state change

--- src/__tests__/components/SearchSchool/SearchSchool.test.js
+++ src/__tests__/components/SearchSchool/SearchSchool.test.js
@@ -16,125 +16,109 @@ const renderWithRedux = (component, store) => {
   )
 }
 
-// learn how to use mockStore instead of recreating the real store
-
 describe('SearchSchool component', () => {
-  it('should render all the components', () => {
-    const store = createStore(
-      rootReducer,
-      {
-        user: {
-          data: {
-            isAuthenticated: false
+  describe('unauthenticted state', () => {
+    it('should render all the components', () => {
+      const store = createStore(
+        rootReducer,
+        {
+          user: {
+            data: {
+              isAuthenticated: false
+            }
           }
-        }
-      }
-    );
-    act(() => {
-      renderWithRedux(<SearchSchool />, store);
-    });
-    expect(screen.getByText(/Search/i)).toBeInTheDocument();
-  });
-
-  it('should dispatch the search action when the Search button is clicked', async () => {
-    const useDispatchMock = jest.spyOn(reactRedux, 'useDispatch').mockReturnValue(jest.fn())
-    const store = createStore(
-      rootReducer,
-      {
-        user: {
-          data: {
-            isAuthenticated: false
-          }
-        }
-      },
-      applyMiddleware(reduxThunk)
-    );
-    act(() => {
-      renderWithRedux(<SearchSchool />, store);
-    });
-    const searchButton = screen.getByText('Search');
-    fireEvent.click(searchButton);
-
-    await waitFor(() => {
-      expect(useDispatchMock).toHaveBeenCalled()
-    })
-  });
-
-  it('should not render the New School button if the user is not authenticated', () => {
-    const store = createStore(
-      rootReducer,
-      {
-        user: {
-          data: {
-            isAuthenticated: false
-          }
-        }
-      }
-    );
-    act(() => {
-      renderWithRedux(<SearchSchool />, store);
-    });
-    expect(screen.queryByText('New School')).not.toBeInTheDocument();
-  })
-
-  it('should render the New School button if the user is authenticated', () => {
-    const store = createStore(
-      rootReducer,
-      {
-        user: {
-          data: {
-            isAuthenticated: true
-          }
-        }
-      }
-    );
-    act(() => {
-      renderWithRedux(<SearchSchool />, store);
-    });
-    expect(screen.queryByText('New School')).toBeInTheDocument();
-  })
-
-  it('should dispatch the openModal action when the New School button is clicked', async () => {
-    const useDispatchMock = jest.spyOn(reactRedux, 'useDispatch').mockReturnValue(jest.fn())
-    const store = createStore(
-      rootReducer,
-      {
-        user: {
-          data: {
-            isAuthenticated: true
-          }
-        }
-      },
-      applyMiddleware(reduxThunk)
+        },
+        applyMiddleware(reduxThunk)
       );
       act(() => {
         renderWithRedux(<SearchSchool />, store);
       });
-      const newSchool = screen.getByText('New School');
-      fireEvent.click(newSchool);
+      expect(screen.getByText(/Search/i)).toBeInTheDocument();
+    });
+
+    it('should dispatch the search action when the Search button is clicked', async () => {
+      const useDispatchMock = jest.spyOn(reactRedux, 'useDispatch').mockReturnValue(jest.fn())
+      const store = createStore(
+        rootReducer,
+        {
+          user: {
+            data: {
+              isAuthenticated: false
+            }
+          }
+        },
+        applyMiddleware(reduxThunk)
+      );
+      act(() => {
+        renderWithRedux(<SearchSchool />, store);
+      });
+      fireEvent.click(screen.getByText('Search'));
+
+      await waitFor(() => {
+        expect(useDispatchMock).toHaveBeenCalled()
+        useDispatchMock.mockClear()
+      })
+    });
+
+    it('should not render the New School button if the user is not authenticated', () => {
+      const store = createStore(
+        rootReducer,
+        {
+          user: {
+            data: {
+              isAuthenticated: false
+            }
+          }
+        }
+      );
+      act(() => {
+        renderWithRedux(<SearchSchool />, store);
+      });
+      expect(screen.queryByText('New School')).not.toBeInTheDocument();
+    })
+  })
+  
+  describe('authenticted state', () => {
+    it('should render the New School button if the user is authenticated', () => {
+      const store = createStore(
+        rootReducer,
+        {
+          user: {
+            data: {
+              isAuthenticated: true
+            }
+          }
+        }
+      );
+
+      act(() => {
+        renderWithRedux(<SearchSchool />, store);
+      });
+      expect(screen.queryByText('New School')).toBeInTheDocument();
+    })
+
+    it('should dispatch the openModal action when the New School button is clicked', async () => {
+      const useDispatchMock = jest.spyOn(reactRedux, 'useDispatch').mockReturnValue(jest.fn())
+      const store = createStore(
+        rootReducer,
+        {
+          user: {
+            data: {
+              isAuthenticated: true
+            }
+          }
+        },
+        applyMiddleware(reduxThunk)
+      );
+
+      act(() => {
+        renderWithRedux(<SearchSchool />, store);
+      });
+      fireEvent.click(screen.getByText('New School'));
       
       await waitFor(() => {
         expect(useDispatchMock).toHaveBeenCalled()
       })
-  });
-
-
-  // xit('should not render the New School button if the user is not authenticated', () => {
-  //   store = mockStore({
-  //     user: {
-  //       data: {
-  //         isAuthenticated: false // Set isAuthenticated to false for testing purposes
-  //       }
-  //     }
-  //   });
-
-  //   component.rerender(
-  //     <Provider store={store}>
-  //       <SearchSchool />
-  //     </Provider>
-  //   );
-
-  //   const newSchoolButton = component.queryByText('New School');
-  //   expect(newSchoolButton).not.toBeInTheDocument();
-  // });
+    });
+  })
 });
